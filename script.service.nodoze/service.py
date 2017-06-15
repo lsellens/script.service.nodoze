95177d3f20625405540a2c362d14e8bab2eb7a9f
lsellens <lsellens@gmail.com>
2017-06-15 17:31:48 -0500

    read proc file instead of calling netstat

--- script.service.nodoze/service.py
+++ script.service.nodoze/service.py
@@ -1,7 +1,5 @@
-from os import popen
 import xbmc
 import xbmcaddon
-import xbmcvfs
 
 __addon__ = xbmcaddon.Addon(id='script.service.nodoze')
 __scriptname__ = "nodoze"
@@ -9,18 +7,11 @@ __author__ = "lsellens"
 __url__ = "https://github.com/lsellens/xbmc.addons"
 __addonpath__ = xbmc.translatePath(__addon__.getAddonInfo('path'))
 __addonhome__ = xbmc.translatePath(__addon__.getAddonInfo('profile'))
-defaultsettings = xbmc.translatePath(__addonpath__ + '/settings-default.xml')
-settings = xbmc.translatePath(__addonhome__ + 'settings.xml')
-
-if not xbmcvfs.exists(settings):
-    xbmcvfs.copy(defaultsettings, settings)
-
 ports = __addon__.getSetting('PORTS')
 ports = ports.replace(" ", "").split(",")
 ipaddress = xbmc.getIPAddress()
-# assumes you're on a class c network
-ipaddresses = ipaddress.rsplit('.', 1)[0] + '.'
 inhibit = False
+xbmc.log('nodoze: Monitoring port(s) ' + str(ports).strip("[]"), level=xbmc.LOGDEBUG)
 
 
 class MyMonitor(xbmc.Monitor):
@@ -31,6 +22,37 @@ class MyMonitor(xbmc.Monitor):
         global ports
         ports = __addon__.getSetting('PORTS')
         ports = ports.replace(" ", "").split(",")
+        xbmc.log('nodoze: Settings Changed. Monitoring port(s) ' + str(ports).strip("[]"), level=xbmc.LOGDEBUG)
+
+
+def read_proc():
+    with open("/proc/net/tcp", 'r') as f:
+        content = f.readlines()
+        content.pop(0)
+    return content
+
+
+def convert_hex(array):
+    ip, port = array.split(':')
+    ip = '.'.join(str(int(i, 16)) for i in reversed([ip[i:i+2] for i in range(0, len(ip), 2)]))
+    port = str(int(port, 16))
+    return ip, port
+
+
+def tcp_port_connection():
+    tcpcontent = read_proc()
+    connected_ports = []
+    for line in tcpcontent:
+        line_array = [x for x in line.split(' ') if x !='']
+        local_ip, local_port = convert_hex(line_array[1])
+        remote_ip, remote_port = convert_hex(line_array[2])
+        state = line_array[3]
+        if local_ip == ipaddress and state == '01' and local_port in ports:
+            nline = '{0} is connected to port {1}'.format(remote_ip, local_port)
+            if nline not in connected_ports:
+                connected_ports.append(nline)
+    return connected_ports
+
 
 if __name__ == '__main__':
     monitor = MyMonitor()
@@ -39,13 +61,15 @@ if __name__ == '__main__':
         if monitor.waitForAbort(60):
             xbmc.executebuiltin('InhibitIdleShutdown(false)')
             break
-        active = popen('netstat -tn | grep -E \'' + ''.join(
-            ['{0}:{1}.*{2}.*ESTABLISHED|'.format(ipaddress, x, ipaddresses) for x in ports]).rstrip('|') + '\'').read()
-
+        
+        active = tcp_port_connection()
+        
         if active:
             xbmc.executebuiltin('InhibitIdleShutdown(true)')
             inhibit = True
-            xbmc.log('nodoze: Preventing sleep\n' + active, level=xbmc.LOGDEBUG)
+            xbmc.log('nodoze: Preventing sleep', level=xbmc.LOGDEBUG)
+            for line in active:
+                xbmc.log('nodoze: ' + line, level=xbmc.LOGDEBUG)
         elif inhibit:
             xbmc.executebuiltin('InhibitIdleShutdown(false)')
             inhibit = False
